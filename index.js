0112362ea58187cdbfc1f83a09defd6cf7ceb767
Aidan <farinaaidan2@gmail.com>
2023-05-22 16:18:17 -0400

    finished working product

---
+++ index.js
@@ -0,0 +1,109 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const README= ({Title, description, installation, usage, contributions, test, LinkedIn, GitHub, email}) => 
+`# ${Title}
+
+
+## Description
+
+
+${description}
+
+
+## Table of Contents
+
+
+* [Installation](./README.md/#installation)
+* [Usage](./README.md/#installation)
+* [Contributions](./README.md/#installation)
+* [Tests](./README.md/#installation)
+* [Questions](./README.md/#installation)
+
+
+## Installation
+
+
+${installation}
+
+
+## Usage
+
+
+${usage}
+
+
+## Contributors
+
+
+${contributions}
+
+
+## Tests
+
+
+${test}
+
+
+## Questions
+
+
+* GitHub: ${GitHub}
+
+
+* LinkedIn: ${LinkedIn}
+
+
+How to reach me for further information: ${email}`;
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'Title',
+      message: 'What is your project title?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Give a description for your project:',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Installation instructions:',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Usage information:',
+      },
+      {
+        type: 'input',
+        name: 'contributions',
+        message: 'Contributions information:',
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'Test instructions:',
+      },
+      {
+        type: 'input',
+        name: 'LinkedIn',
+        message: 'What is the link to your LinkedIn?',
+      },
+      {
+        type: 'input',
+        name: 'GitHub',
+        message: 'What is the link to your GitHub?',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address:',
+      },
+])
+.then((answers) => {
+    fs.writeFile('README.md', README(answers), (err) => 
+        err ? console.log(err) : console.log('Successfully created README!')
+    )
+})
